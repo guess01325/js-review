182cce5e6691267b719418d8ba0cc8d84a71d8a4
Otis Guess <guess01325@gmail.com>
2020-07-20 17:17:03 -0400

    [cowboys function]

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # JavaScriptReview
 This is my Review
+
+
+## Examples of Data Code
+
+```javascript
+
+```const dallasCowboys = {
+    Quarterback: 'Dak Prescott',
+    Owner: 'Jerry Jones',
+    superbowls: 5,
+    organization: ["jerry Jones", 'Mike Mcarthy'],
+    Rivals: {
+        rivalOne = 'Eagles',
+        rivalTwo = 'Redskins',
+        rivalThree = 'Giants',
+    }
+}
+console.log(dallasCowboys)```
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const friends =['kevin', 'molik','dom', 'jason','asia'];
 
 const tesla = {
     insustry: 'Electric Cars',
-    founder: 'Elon Musk',
+    ceo: 'Elon Musk',
     yearFounded: 2003,
     foundingMembers: ['Elon Musk', 'JB Straubel', 'Martin Eberated'],
     stockTicker: 'TSLA';
@@ -13,8 +13,27 @@ const tesla = {
         vehicleOne: 'Model S',
         vehicleTwo: 'Mdel x',
         vehicleThree: 'Model 3'
+    },
+    print: function(){
+        console.log('The CEO of Tesla is ' + this.ceo);
     }
 }
+
+const dallasCowboys = {
+    Quarterback: 'Dak Prescott',
+    Owner: 'Jerry Jones',
+    superbowls: 5,
+    organization: ["jerry Jones", 'Mike Mcarthy'],
+    Rivals: {
+        rivalOne = 'Eagles',
+        rivalTwo = 'Redskins',
+        rivalThree = 'Giants',
+    }
+}
+console.log(dallasCowboys);
+
+
+tesla
 
 console.log(name);
 console.log(age);
@@ -22,4 +41,5 @@ console.log(isCool);
 console.log(friends);
 console.log(tesla);
 console.log(tesla.vehicles.vehicleThree);
-console.log(tesla['vehicles']{'vechicleThree'});
+console.log(tesla['vehicles']['vechicleThree']);
+tesla.print
