9bdf2d4c19c45fdea7ce2771894b1919486019ce
Otis Guess <guess01325@gmail.com>
2020-07-20 17:00:27 -0400

    [codelange java Review]

---
+++ app.js
@@ -0,0 +1,25 @@
+let name = 'Otis Guess'
+let age = 34;
+let isCool = true;
+const friends =['kevin', 'molik','dom', 'jason','asia'];
+
+const tesla = {
+    insustry: 'Electric Cars',
+    founder: 'Elon Musk',
+    yearFounded: 2003,
+    foundingMembers: ['Elon Musk', 'JB Straubel', 'Martin Eberated'],
+    stockTicker: 'TSLA';
+    vehicles: {
+        vehicleOne: 'Model S',
+        vehicleTwo: 'Mdel x',
+        vehicleThree: 'Model 3'
+    }
+}
+
+console.log(name);
+console.log(age);
+console.log(isCool);
+console.log(friends);
+console.log(tesla);
+console.log(tesla.vehicles.vehicleThree);
+console.log(tesla['vehicles']{'vechicleThree'});
